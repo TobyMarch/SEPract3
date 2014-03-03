301e97b4cd36175effcf1c4271313dab9a38e185
cchestnut91 <cchestnut91@gmail.com>
2014-03-02 20:57:58 -0500

    Fixed problem where Modification would fail because Person with ID already exists.

--- ModifyDataViewController.m
+++ ModifyDataViewController.m
@@ -56,6 +56,45 @@
     [self.idNumber setText:[NSString stringWithFormat:@"%d",[thePerson ID]]];
 }
 
+- (IBAction)createPerson:(id)sender {
+    BOOL added=NO;
+    BOOL emptyField=NO;
+    if (self.gradYearLabel.hidden == NO) {
+        if(!([self.firstName.text isEqualToString:@""] || [self.lastName.text isEqualToString:@""] || (self.idNumber.text.intValue==0) || (self.sliderValue.text.doubleValue==0.0) || (self.graduationYear.text.intValue==0) || [self.majorBox.text isEqualToString:@""])){
+            [self.list removePerson:self.idNumber.text.intValue];
+            added = [self.list addStudentWithMajor:self.majorBox.text andYear:self.graduationYear.text.intValue andGpa:self.sliderValue.text.doubleValue andFirstName:self.firstName.text andLastName:self.lastName.text andID:self.idNumber.text.intValue];
+        }
+        else{
+            emptyField=YES;
+        }
+    }
+    else {
+        if(!([self.firstName.text isEqualToString:@""] || [self.lastName.text isEqualToString:@""] || (self.idNumber.text.intValue==0) || [self.departmentName.text isEqualToString:@""])){
+            [self.list removePerson:self.idNumber.text.intValue];
+            added = [self.list addProfessorWithSalary:self.sliderValue.text.doubleValue andTenured:self.tenureStatus.isOn andDept:self.departmentName.text andFirstName:self.firstName.text andLastName:self.lastName.text andID:self.idNumber.text.intValue];
+        }
+        else{
+            emptyField=YES;
+        }
+    }
+    if(!added) {
+        if(emptyField){
+            UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Add Person Failed" message:@"All fields must have a value" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            [errorAlert show];
+        }
+        else{
+            UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Add Person Failed" message:@"Person with that ID already exists" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            [errorAlert show];
+        }
+    }
+    else{
+        UIAlertView *successAlert = [[UIAlertView alloc] initWithTitle:@"Added Person" message:[NSString stringWithFormat:@"%@ added!", self.firstName.text] delegate:self cancelButtonTitle:@"OK!" otherButtonTitles:nil, nil];
+        [successAlert show];
+    }
+    //An 'else' case is needed here, but I don't know how to implement it yet
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
